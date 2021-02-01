508d92f8249bc0944085d17aa57bdded1a78fbe9
asif <mdasif08737@gmail.com>
2021-02-02 00:19:33 +0530

    Initial commit

---
+++ news_reader.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from win32com.client import Dispatch
+
+def speak_news(articles):
+    speak = Dispatch('SAPI.SpVoice')
+
+    # iterating the articles by one by one and pass to speak module to speak
+    for i in range(len(articles)):
+        article = (i+1,articles[i])
+        speak.Speak(article)
+
+
+if __name__ == '__main__':
+    #geting the data using news api news api
+    news = requests.get("https://newsapi.org/v2/top-headlines?source=bbc_news&country=us&apiKey=f44fe3073fa348b488d0c8ba983d8ef9")
+    # parsing the data into json format
+    json_news = news.json()
+
+    #getting articles from the json file
+    articles = json_news["articles"]
+
+    #creating a empty list to store the titles from articles
+    articles_list = []
+
+    for article in articles:
+        articles_list.append(article["title"])
+
+
+    #calling the speak function and passing the list of title
+    speak_news(articles_list)
+
+
